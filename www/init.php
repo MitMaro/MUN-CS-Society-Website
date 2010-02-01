f3374a20cc6e4465e0846e1b15cbd7396470817c
Tim Oram <mitmaro@mitmaro.ca>
2010-01-31 21:54:37 -0330

    Added the initialization reference file

---
+++ www/init.php
@@ -0,0 +1,12 @@
+<?php
+/*------------------------------------------------------------------------------
+    File: www/init.php
+ Project: MMTiny
+ Version: 0.1.0
+      By: Tim Oram [t.oram@mitmaro.ca]
+ Website: http://www.mitmaro.ca/
+   Email: mmtiny@mitmaro.ca
+ Purpose: Initialization reference 
+------------------------------------------------------------------------------*/
+// References the main init.php
+require dirname(__FILE__) . '/../php/init.php';
