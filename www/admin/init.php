6d2a002164e5d636ea6f3a61797dd070d9f1aae9
Tim Oram <mitmaro@mitmaro.ca>
2010-02-14 00:17:27 -0330

    Added the admin initialization reference file

---
+++ www/admin/init.php
@@ -0,0 +1,12 @@
+<?php
+/*------------------------------------------------------------------------------
+    File: www/admin/init.php
+ Project: MUN Computer Science Society Website
+ Version: 0.1.0
+      By: Tim Oram
+ Website: http://society.cs.mun.ca
+   Email: muncssociety@gmail.com
+ Purpose: Initialization reference 
+------------------------------------------------------------------------------*/
+// References the main init.php
+require dirname(__FILE__) . '/../../php/init.php';
