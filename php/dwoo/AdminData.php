cdf4b597f74546a5b1dc682790327548ed88c789
Tim Oram <mitmaro@mitmaro.ca>
2010-02-28 00:47:57 -0330

    Added the admin navigation to the dwoo admin data object

--- php/dwoo/AdminData.php
+++ php/dwoo/AdminData.php
@@ -11,8 +11,28 @@
 class AdminData extends BaseData {
 	public function __construct(){
 		parent::__construct();
-		$this->assign('site', array(
-			'messages' => Session::getOnce('messages', array())
-		));
+		$this->assign('user', Session::getUserAll());
+
+		if(User::isLogged()){
+			
+			// the admin navigation
+			// first check to see if the user can view all admin pages, this should
+			// speed things up in certain circumstances 
+			$a = new Action('admin/view');
+
+			if($a->checkActionAllowedByUser(Session::getUser('id', 1), Session::getUser('group', 1))){
+				$nav = DBAdmin::getAdminNavigation();
+			}
+			else{
+				$nav = array();
+				foreach(DBAdmin::getAdminNavigation() as $n){
+					$a = new Action('admin/view/' . $n['name']);
+					if($a->checkActionAllowedByUser(Session::getUser('id', 1), Session::getUser('group', 1))){
+						$nav[] = $n;
+					}
+				}
+			}
+			$this->assign('nav', $nav);
+		}
 	}
 }
