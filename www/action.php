65c0bedb97f171755e10de25aebd1cd33f5e7ca0
Tim Oram <mitmaro@mitmaro.ca>
2010-02-28 01:00:34 -0330

    Modified the action controller to use the new action class and added some other features

--- www/action.php
+++ www/action.php
@@ -10,52 +10,62 @@
           load the request handler for the action provided.
 ------------------------------------------------------------------------------*/
 require 'init.php';
+if(isset($_GET['_action']) && preg_match('|^[0-9a-z_]+(/[a-z0-9_]+)*$|', $_GET['_action'])) {
 
-if(isset($_GET['_action']) && preg_match('/^[a-zA-Z]+$/', $_GET['_action'])) {
-	$action = strtolower($_GET['_action']);
-	$command = strtolower($_GET['_cmd']);
-	$class = 'Request' . ucfirst($action);
-	if(isset($_GET['_admin'])){
-		$path = 'php/actions/admin/' . $action . '.php';
-		$action = 'admin/' . $action;
-	}
-	else{
-		$path = 'php/actions/' . $action . '.php';
-	}
-	
-	if(file_exists($cfg['project_root'] . $path)){
+	$action = new Action(strtolower($_GET['_action']));
+
+	$path = $action->getActionAsPath();
+	$class= $action->getActionClassName();
+	if($action->isValid() && file_exists($cfg['project_root'] . $path)){
 		require $path;
 	
 		$request = new $class;
-	
-		if(!User::verifyAccess($action . '/' . $command)){
+		
+		// default id of 1 is the guest user, later I would like to grab this
+		// info from the database
+		if(!$action->checkActionAllowedByUser(Session::getUser('id', 1), Session::getUser('group', 1))){
 			$request->setStatus(Request::UNAUTHORIZED);
 		}
 		else {
-			$request->process($command);
+			$request->process($action->getCommand());
 		}
 	}
 	else{
 		$request = new Request();
-		$action = 'none';
 		$request->setStatus(Request::NOT_IMPLEMENTED);
 	}
 }
 else {
+	$action = new Action("none");
 	$request = new Request();
-	$action = 'none';
 	$request->setStatus(Request::ACTION_REQUIRED);
 }
-if(Request::isAjaxRequest()){
+if(Request::isAjaxRequest() || isset($_GET['_json'])){
 	header('Content-type: application/json');
 	header('Cache-Control: no-cache');
 	header('Pragma: no-cache');
 	echo $request->json();
 }
 else{
-	Session::setOnce('action', $action);
+	Session::setOnce('action', $action->getActionName());
 	Session::setOnce('status', $request->getStatus());
 	Session::setOnce('data', $request->getData());
+
+	// handle some common return codes from the request class
+	$msg = $request->getStatus();
+	if($msg['code'] === Request::OK){
+		Session::addMessage('Action completed successfully');
+	}
+	else if($msg['code'] === Request::INVALID_DATA){
+		Session::addMessage('Invalid data was provided, please correct and try again');
+	}
+	else if($msg['code'] === Request::UNAUTHORIZED){
+		Session::addMessage('You are not authorized to perform that action');
+	}
+	else {
+		Session::addMessage('An error has occured: ' . $msg['message']);
+	}
+	
 	if(isset($_REQUEST['request_page'])){
 		$redirect = $cfg['site_path'] . trim($_REQUEST['request_page'], '/');
 	}
